727fb14288569afc8ef240d90e6cdf450d6b9fb7
tanvir-ux <tanviralamshuvo@gmail.com>
2021-01-18 03:40:36 +0600

    Proxy Pattern Done

---
+++ proxyPattern/ProxyPatternDemo.java
@@ -0,0 +1,9 @@
+public class ProxyPatternDemo {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test.jpg");
+        image.display();
+        System.out.println("");
+        image.display();
+    }
+    
+}
