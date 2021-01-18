957972632a90bc4dfb3f10deb1ff0b5750daad0d
tanvir-ux <tanviralamshuvo@gmail.com>
2021-01-18 21:46:06 +0600

    State Pattern Done

---
+++ statePattern/StatePatternDemo.java
@@ -0,0 +1,17 @@
+public class StatePatternDemo {
+    public static void main(String[] args) {
+        Context context = new Context();
+        StartState startState = new StartState();
+        startState.doAction(context);
+        System.out.println(context.getState().toString());
+        StopState stopState = new StopState();
+        stopState.doAction(context);
+        System.out.println(context.getState().toString());
+    }
+}
+
+// Output:
+// Player is in start state
+// Start State
+// Player is in stop state
+// Stop State
